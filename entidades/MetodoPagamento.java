fcd831109556b855fb004e2bfdf7e0a88c60d99e
nerylucas <97478323+nerylucas@users.noreply.github.com>
2025-12-01 20:10:35 -0300

    Incluido excecao no metodo de pagamento
    Updated processarPagamento method to throw SaldoInsuficienteException.

--- entidades/MetodoPagamento.java
+++ entidades/MetodoPagamento.java
@@ -1,6 +1,9 @@
 package entidades;
 
+import excecoes.*;
+
 public interface MetodoPagamento {
-	void processarPagamento(double valor);
+	void processarPagamento(double valor) throws SaldoInsuficienteException;
 
 }
+
