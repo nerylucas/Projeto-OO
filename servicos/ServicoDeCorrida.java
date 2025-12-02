fb3bbc4427d00348190f3ba7069b4163bb0512c1
marrathomaz <marrathomaz02@gmail.com>
2025-12-02 03:16:04 -0300

    Update ServicoDeCorrida.java

--- servicos/ServicoDeCorrida.java
+++ servicos/ServicoDeCorrida.java
@@ -1,35 +1,69 @@
 package servicos;
-import entidades.Corrida;
-import entidades.Passageiro;
-import entidades.Motorista;
-import java.lang.Exception;
-import entidades.Categoria;
-import entidades.MetodoPagamento;
-
+import entidades.*;
+import excecoes.*;
 public class ServicoDeCorrida {
 
-	public Corrida solicitarCorrida(Passageiro p, String origem, String destino, Categoria cat) {
-	
-		System.out.println("O passageiro "+p.getNome()+" solicitou uma corrida de "+ origem +" até "+destino+ " pela categoria " + cat);
+	public Corrida solicitarCorrida(Corrida c, Passageiro p, String origem, String destino, Categoria cat) throws PassageiroPendenteException {
+		if(p.isEmDebito()) {
+			throw new PassageiroPendenteException("O passageiro não pode solicitar uma nova viagem enquanto possui pendências.");
+		}
+		else {
+		c.setStatus(StatusCorrida.SOLICITADA);
+		System.out.println("Corrida do passageiro " +p+" solicitada! A viagem saíra de " +origem+" e irá até " +destino+"! A viagem será realizada na categoria "+cat+"!");
+		}
 		return null;
-	}
-	public void aceitarCorrida(Corrida c, Motorista m) {
+		}
 		
+	
+	public void aceitarCorrida(Corrida c, Motorista m) throws NenhumMotoristaDisponivelException {
+		if(m.getStatus() != StatusMotorista.ONLINE) {
+			throw new NenhumMotoristaDisponivelException("Nenhum motorista disponivel");
+		}
+		else {
+			c.setStatus(StatusCorrida.ACEITA);
+			m.setStatus(StatusMotorista.EM_CORRIDA);
+		}
 	}
-	public void iniciarCorrida(Corrida c, double km) {
-		
+	public void iniciarCorrida(Corrida c) throws EstadoInvalidoDaCorridaException {
+		if (c.getStatus() != StatusCorrida.ACEITA) {
+        throw new EstadoInvalidoDaCorridaException("Corrida não está no estado ACEITA.");
+    }
+		else {
+		    c.iniciarViagem();
+		    c.setStatus(StatusCorrida.EM_ANDAMENTO);
+		}
+		}
+	   
+	public void finalizarCorrida(Corrida c, double km) throws EstadoInvalidoDaCorridaException, PagamentoRecusadoException, SaldoInsuficienteException {
+
+        if (c.getStatus() != StatusCorrida.EM_ANDAMENTO) {
+            throw new EstadoInvalidoDaCorridaException("Corrida não está EM_ANDAMENTO.");
+        }
+        
+        double valorFinal = c.getCategoria().calcularPreco(km);
+        c.setDistanciaKm(km);
+        c.setValorFinal(valorFinal);
+		    
+         try {
+        	    c.getMetodoPagamento().processarPagamento(valorFinal);
+        	 } catch (SaldoInsuficienteException e) {
+        	     throw new PagamentoRecusadoException("Pagamento recusado: " + e.getMessage());
+        	 } 
 	}
-	public void finalizarCorrida(Corrida c, double km) {
-		
+	public void cancelarCorrida(Corrida c, double km, Motorista m) throws EstadoInvalidoDaCorridaException {
+		if(c.getStatus() == StatusCorrida.FINALIZADA) {
+		throw new EstadoInvalidoDaCorridaException("Não existe nenhuma corrida solicitada ou em andamento.");
 	}
-	public void cancelarCorrida(Corrida c, double km) {
-		
+	else {
+			c.setStatus(StatusCorrida.FINALIZADA);
+			m.setStatus(StatusMotorista.ONLINE);
 	}
-	public void processarPagamento(Corrida c, MetodoPagamento mp) throws Exception{
-		
-	}
+		}
+	
+	
 	}
 	
 	
 	
+
 
